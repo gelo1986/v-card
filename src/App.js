e403f26e619c501375ccb6b3b620b42a2ecf17e1
Oleg Tiulenev <oleg.tiulenev@sms-online.com>
2018-04-12 15:04:28 +0300

    .

--- src/App.js
+++ src/App.js
@@ -75,12 +75,13 @@ class App extends PureComponent {1
             <a className='msges Tgm' title="Telegram" href="tg://resolve?domain=nickname">
               <p className='msgesText'>{this.state.lang.tgm}</p>
             </a>
-           <a className='msges Whp' title="WhatsApp" href="whatsapp://send?phone=+79153602266">
-            <p className='msgesText'>{this.state.lang.wha}</p>
-          </a>
           <a className='msges Vbr' title="Viber" href="viber://chat?number=+79153602266">
             <p className='msgesText'>{this.state.lang.vbr}</p>
           </a>
+           <a className='msges Whp' title="WhatsApp" href="whatsapp://send?phone=+79153602266">
+            <p className='msgesText'>{this.state.lang.wha}</p>
+          </a>
+          
           <div className='msges Mail' onClick={this.sendMail}>
             <p className='msgesText'>{this.state.lang.msg}</p>
           </div>
